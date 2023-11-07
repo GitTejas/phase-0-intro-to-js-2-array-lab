4c7f18e92b36f8236a60cf66a1f178257de6b74d
Tejas Nampalli <TSNampalli@gmail.com>
2023-11-07 18:01:39 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(name)
+}
+
+function appendCat(name) {
+    const newCats = [...cats, name]
+    return newCats
+}
+
+function prependCat(name) {
+    const newCats = [name, ...cats]
+    return newCats
+}
+
+function removeLastCat() {
+    const noLast = cats.slice(0, 2)
+    return noLast
+}
+
+function removeFirstCat() {
+    const noFirst = cats.slice(1)
+    return noFirst
+}
